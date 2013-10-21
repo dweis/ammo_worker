13c4313f2fab83721d468f0f819d90e90028f01a
Derrick Weis <derrick@derrickweis.com>
2013-10-21 08:50:49 -0400

    add start/stop simulation methods

--- js/ammo_worker.js
+++ js/ammo_worker.js
@@ -2,7 +2,8 @@
   function AmmoWorker(opts) {
     var context = this, i, apiMethods = [
           'on', 'fire', 'addRigidBody', 'swap',
-          'setStep', 'setIterations', 'setGravity'
+          'setStep', 'setIterations', 'setGravity',
+          'startSimulation', 'stopSimulation'
         ];
 
     opts = opts || {};
@@ -43,10 +44,6 @@
       
       importScripts('./js/ammo.js');
 
-      var meanDt = 0, meanDt2 = 0, frame = 1;
-
-      var trans = new Ammo.btTransform();
-      
       this.bodies = [];
 
       this.collisionConfiguration = new Ammo.btDefaultCollisionConfiguration();
@@ -57,56 +54,67 @@
           this.overlappingPairCache, this.solver, this.collisionConfiguration);
       
       this.fire('ready');
+    },
+
+    startSimulation: function() {
+      var that = this,
+          trans = new Ammo.btTransform(),
+          meanDt = 0, meanDt2 = 0, frame = 1,
+          last;
+
+      this.buffers = [
+        new ArrayBuffer(this.maxBodies * 7 * 8),
+        new ArrayBuffer(this.maxBodies * 7 * 8),
+        new ArrayBuffer(this.maxBodies * 7 * 8)
+      ];
       
-      if (this.dynamicsWorld) {
-        this.buffers = [
-          new ArrayBuffer(this.maxBodies * 7 * 8),
-          new ArrayBuffer(this.maxBodies * 7 * 8),
-          new ArrayBuffer(this.maxBodies * 7 * 8)
-        ];
+      last = Date.now();
+      this.simulationTimerId = setInterval(function() {
+        var now = Date.now();
+        var dt = now - last || 1;
+        that.dynamicsWorld.stepSimulation(dt, that.iterations);
+          var alpha;
+          if (meanDt > 0) {
+            alpha = Math.min(0.1, dt/1000);
+          } else {
+            alpha = 0.1; // first run
+          }
+          meanDt = alpha*dt + (1-alpha)*meanDt;
+
+          var alpha2 = 1/frame++;
+          meanDt2 = alpha2*dt + (1-alpha2)*meanDt2;
+        last = Date.now();
         
-        var last = Date.now();
-        setInterval(function() {
-          var now = Date.now();
-          var dt = now - last || 1;
-          that.dynamicsWorld.stepSimulation(dt, that.iterations);
-            var alpha;
-            if (meanDt > 0) {
-              alpha = Math.min(0.1, dt/1000);
-            } else {
-              alpha = 0.1; // first run
-            }
-            meanDt = alpha*dt + (1-alpha)*meanDt;
+        var update;
+        
+        if (that.buffers.length > 0) {
+          update = new Float64Array(that.buffers.pop());    
+        }
+        
+        if (update && update.buffer instanceof ArrayBuffer) {
+          for (var i in that.bodies) {
+            if (that.bodies[i]) {
+              that.bodies[i].getMotionState().getWorldTransform(trans);
 
-            var alpha2 = 1/frame++;
-            meanDt2 = alpha2*dt + (1-alpha2)*meanDt2;
-          last = Date.now();
-          
-          var update;
-          
-          if (that.buffers.length > 0) {
-            update = new Float64Array(that.buffers.pop());    
-          }
-          
-          if (update && update.buffer instanceof ArrayBuffer) {
-            for (var i in that.bodies) {
-              if (that.bodies[i]) {
-                that.bodies[i].getMotionState().getWorldTransform(trans);
-
-                update[i * 7 + 0] = trans.getOrigin().x();
-                update[i * 7 + 1] = trans.getOrigin().y();
-                update[i * 7 + 2] = trans.getOrigin().z();
-                update[i * 7 + 3] = trans.getRotation().x();
-                update[i * 7 + 4] = trans.getRotation().y();
-                update[i * 7 + 5] = trans.getRotation().z();
-                update[i * 7 + 6] = trans.getRotation().w();
-              }
+              update[i * 7 + 0] = trans.getOrigin().x();
+              update[i * 7 + 1] = trans.getOrigin().y();
+              update[i * 7 + 2] = trans.getOrigin().z();
+              update[i * 7 + 3] = trans.getRotation().x();
+              update[i * 7 + 4] = trans.getRotation().y();
+              update[i * 7 + 5] = trans.getRotation().z();
+              update[i * 7 + 6] = trans.getRotation().w();
             }
-    
-            that.fire('update', update.buffer, [update.buffer]);
-            that.fire('stats', { currFPS: Math.round(1000/meanDt), allFPS: Math.round(1000/meanDt2) });
           }
-        }, this.step * 1000);
+  
+          that.fire('update', update.buffer, [update.buffer]);
+          that.fire('stats', { currFPS: Math.round(1000/meanDt), allFPS: Math.round(1000/meanDt2) });
+        }
+      }, this.step * 1000);
+    },
+
+    stopSimulation: function() {
+      if (this.simulationTimerId) {
+        clearInterval(this.simulationTimerId);
       }
     },
     
@@ -172,6 +180,11 @@
       if (typeof fn === 'function') {
         fn(idx);
       }
+    },
+
+    removeRigidBody: function(id) {
+      this.dynamicsWorld.removeRigidBody(this.bodies[id]);
+      Ammo.destroy(this.bodies[id]);
     },
     
     shutdown: function() {
